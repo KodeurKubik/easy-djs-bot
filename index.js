53d5fd0655d21b5dd7ac00ea1c5a8024b63ee37b
TotoroGaming <84036232+TotoroGaming@users.noreply.github.com>
2021-12-03 21:15:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 // Command
 exports.createCommand = require('./src/client/createCommand');
 exports.waitForCommand = require('./src/client/MainFunctions').waitForCommand;
+exports.pause = require('./src/client/MainFunctions').pause;
+exports.unpause = require('./src/client/MainFunctions').unpause;
+exports.setHelpCategory = require('./src/client/MainFunctions').setHelpCategory;
 
 // Channel
 exports.channel = require('./src/channel/index');
 
 // Utilities
-exports.version = require('../package.json').version;
+exports.version = require('./package.json').version;
 exports.setPrefix = require('./src/client/MainFunctions').setPrefix;
 exports.setToken = require('./src/client/MainFunctions').setToken;
