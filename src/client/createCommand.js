5f5ffa1059104899a0c2c846e2d6fbcb085b01cd
TotoroGaming <84036232+TotoroGaming@users.noreply.github.com>
2021-12-03 21:15:29 +0100

    Update createCommand.js

--- src/client/createCommand.js
+++ src/client/createCommand.js
@@ -2,8 +2,8 @@
 
 const { addCommand } = require('./MainFunctions')
 
-async function createCommand({ name, reply, ping, permissions, execute, ignorePause }, prefix) {
-    addCommand({ name, reply, ping, permissions, execute, ignorePause }, prefix)
+async function createCommand({ name, reply, category, ping, permissions, description, execute, ignorePause }, prefix) {
+    addCommand({ name, reply, category, ping, permissions, execute, ignorePause, description }, prefix)
 }
 
 module.exports = createCommand;
